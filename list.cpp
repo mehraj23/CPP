8699a948826151a60098abb69febf56f7c26d48d
mehraj23 <mehraj.shaikh24@gmail.com>
2025-03-24 03:39:03 +0530

    Used iterator in list

--- list.cpp
+++ list.cpp
@@ -12,8 +12,18 @@ int main()
         cout<<x<<" ";
     }
 
+    // adding an element to the list
+    l.push_back(6);
+
     //printing it other way
     for (auto it=l.begin();it!=l.end();it++)
+    {
+        cout<<*it<<" ";
+    }
+
+    // printing using iterator
+    list<int>::iterator it;
+    for(it=l.begin();it!=l.end();it++)
     {
         cout<<*it<<" ";
     }
