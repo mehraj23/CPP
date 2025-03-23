e18b51885fff5af8eab38cbd9edeb14aa2c1cc80
mehraj23 <mehraj.shaikh24@gmail.com>
2025-03-24 03:59:17 +0530

    Removed element from map

--- map.cpp
+++ map.cpp
@@ -9,11 +9,11 @@ int main()
     map<int, string> m;
 
     //inserting elements in map
-    m[1]="Mehraj";
-    m[2]="Fayaz";
-    m[3]="Azaz";
-    m[4]="Toufik";
-    m[5]="Samrin";
+    m[1]="A";
+    m[2]="B";
+    m[3]="C";
+    m[4]="D";
+    m[5]="E";
 
     //printing map auto way
     for(auto x:m)
@@ -22,7 +22,7 @@ int main()
     }
 
     //inseting elements in map
-    m.insert(pair<int,string>(6,"Saleha"));
+    m.insert(pair<int,string>(6,"F"));
     cout<<endl;
     //printing map using iterator
     map<int,string>::iterator it;
@@ -33,14 +33,32 @@ int main()
     }
 
     //inseting another element in map
-    m.insert(pair<int,string>(7,"Arshaan"));
+    m.insert(pair<int,string>(7,"G"));
 
+    
+    //printing size of map
+    cout<<m.size()<<endl;
+
+    //adding element using key
+    m[8]="H";
+    
+    
     //printing map using iterator
     map<int,string>::iterator it1;
 
     for(it1=m.begin();it1!=m.end();it1++)
     {
         cout<<it1->first<<" " <<it1->second<<endl;
+    }
+
+    //removing element from map
+    m.erase(8);
+
+    //printing map using iterator
+    map<int,string>::iterator it2;
+    for(it2=m.begin();it2!=m.end();it2++)
+    {
+        cout<<it2->first<<" " <<it2->second<<endl;
     }
 
 }
